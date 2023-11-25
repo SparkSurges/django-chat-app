38941f75781a52ff8a3ba8bbc5378deafb2356b0
luluopa <proluluopa@proton.me>
2023-11-25 08:08:03 -0300

    feat: adding complete chat function

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,7 +3,7 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
-from chat.models import Chat
+from chat.models import Chat, Message
 
 class ChatConsumer(WebsocketConsumer):
 
@@ -12,14 +12,30 @@ class ChatConsumer(WebsocketConsumer):
         self.room_name = None
         self.room_group_name = None
         self.room = None
+        self.name = None
 
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
         self.room = Chat.objects.get(name=self.room_name)
+        self.user = self.scope['user']
 
         self.accept()
 
+        if self.user.is_authenticated:
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'user_join',
+                    'user': self.user.username
+                }
+            )
+            self.room.join(self.user)
+
+        self.send(json.dumps({
+            'type': 'user_list',
+            'users': [user.username for user in self.room.online.all()],
+        }))
         # Joining the channels group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -31,18 +47,43 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             self.channel_name,
         )
+
+        if self.user.is_authenticated:
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'user_leave',
+                    'user': self.user.username,
+                }
+            )
+            self.room.leave(self.user)
     
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        if not self.user.is_authenticated:
+            return
+
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
+                'user': self.user.username,
                 'message': message,
             }
         )
 
+        Message.objects.create(user=self.user, chat=self.room, content=message)
+
     def chat_message(self, event):
+        self.send(text_data=json.dumps(event))
+    
+    def user_list(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def user_join(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def user_leave(self, event):
         self.send(text_data=json.dumps(event))
--- core/asgi.py
+++ core/asgi.py
@@ -2,13 +2,16 @@ import os
 import chat.routing 
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter(
-        chat.routing.websockets_urlpatterns
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websockets_urlpatterns
+        )
     ), 
 })

863c0abbdb113df17dabf13e828d5655b258e7ea
luluopa <proluluopa@proton.me>
2023-12-07 15:39:14 -0300

    feat: adding upload-image path

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from user.views import login_view, registration_view, logout_view
+from user.views import (
+    login_view, 
+    registration_view, 
+    logout_view,
+    update_user_view
+)
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', registration_view, name='register'),
+    path('upload-image/', update_user_view, name='update-profile')
 ]
